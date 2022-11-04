95c687aaf73fb371c083f3ca7374e74f29dc5d78
Manoj <manoj.chavva9501@gmail.com>
2022-11-04 15:08:58 +0530

    Project

---
+++ pwm/.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLEExceptions.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLEExceptions.cpp.o: \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
---
+++ pwm/.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLESecurity.cpp.d
@@ -0,0 +1,8 @@
+.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLESecurity.cpp.o: \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.cpp \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.h \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/manoj/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
