5b9c742842fbe18365923b9ba8513ab4a2307648
Manoj <manoj.chavva9501@gmail.com>
2022-11-05 09:15:55 +0530

    file change

--- pwm/.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /home/manoj/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/manoj/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- pwm/.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32doit-devkit-v1/libdbf/BLE/BLEExceptions.cpp.o: \
- /home/manoj/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
